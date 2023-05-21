7ad72f61e5e043b3e4f932d14ec379584ac53013
Lucas91z <lucaszapata.2410@gmail.com>
2023-05-21 20:33:32 -0300

    pasado a limpio

---
+++ validar-form.js
@@ -0,0 +1,55 @@
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const mensaje = document.getElementById("mensaje");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    checkInputs();
+});
+
+
+function checkInputs() {
+    const nombreValue = nombre.value.trim();
+    const emailValue = email.value.trim();
+    const mensajeValue = mensaje.value.trim();
+
+    if (nombreValue === "") {
+        setErrorFor(nombre, "Ingrese un nombre.");
+    } else {
+        setSuccessFor(nombre);
+    }
+
+    if (emailValue === "") {
+        setErrorFor(email, "Ingrese un e-mail.");
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, "No es un email válido.");
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (mensajeValue === "") {
+        setErrorFor(mensaje, "Escriba un mensaje.");
+    } else {
+        setSuccessFor(mensaje);
+    }
+
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+    formControl.className = "form-control error";
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success"
+}
+
+function isEmail(email) {
+    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+}
+
+console.log
