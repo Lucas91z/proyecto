90b921d5a2b41b3d4e62a41e323b79fb72a5987a
Lucas91z <lucaszapata.2410@gmail.com>
2023-05-21 18:59:52 -0300

    validacion

--- proyecto-main - prueba/validar-form.js
+++ proyecto-main - prueba/validar-form.js
@@ -1,40 +1,37 @@
-const nombre = document.getElementById("nombre");
-const tel = document.getElementById("tel");
-const email = document.getElementById("email");
-const mensaje = document.getElementById("mensaje");
 const form = document.getElementById("form");
-const alerta = document.getElementById("alerta");
+const nombre =document.getElementById("nombre");
+const email = document.getElementById("email");
+const mensaje = document.getAnimations("mensaje");
 
-form.addEventListener("submit", e => {
+form.addEventListener("sumbit", e => {
     e.preventDefault();
-    let alerta_msg = "";
-    let entrar = false;
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    let regexTel = /^(?:[0-9] ?){6,14}[0-9]$/;
-
-    if (nombre.value.length < 2) {
-        alerta_msg += "El nombre es muy corto<br>";
-        entrar = true;
-    }else if (nombre.value.length > 20) {
-        alerta_msg += "El nombre es demasiado largo (máximo 20 caracteres)<br>";
-        entrar = true;
-    }
-    if (!regexTel.test(tel.value)) {
-        alerta_msg += "Inserte un teléfono válido<br>";
-        entrar = true;
-    }
-    if (!regexEmail.test(email.value)) {
-        alerta_msg += "Inserte un email válido <br>";
-        entrar = true;
-    }
-    if (mensaje.value == "") {
-        alerta_msg += "Escriba un mensaje";
-        entrar = true;
-    }
-
-    if (entrar) {
-        alerta.innerHTML = "<span style='color: red'>" + alerta_msg + "</span>";
-    } else {
-        alerta.innerHTML = "<span style='color: green'>Mensaje enviado</span>";
-    }
+    checkInputs();
 });
+
+function checkInputs() {
+    const nombreValue = nombre.value.trim();
+    const emailValue = email.value.trim();
+    const mensajeValue = mensaje.value.trim();
+
+    if (nombreValue === ""){
+        setErrorFor(nombre, "Ingrese un nombre");
+    }else{
+        setSuccessFor(nombre);
+    }
+}
+
+function setErrorFor (input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+    formControl.className = "form-control error";
+    small.innerText = message;
+}
+
+function setSuccessFor (input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success"
+}
+
+function isEmail (email) {
+    return /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+}
